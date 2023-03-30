327805d2fa3e14232ecf1655a9b619f649a373a3
li.hongming <sunshine1028@foxmail.com>
2023-03-30 17:40:12 +0800

    复习一遍：中缀表达式和后缀表达式互转
    设计模式类型：
    创建型模式：单例模式、工厂方法、抽象工厂、静态工厂、建造者、原型模式
    结构性模式：适配器、桥接（桥梁模式）、组合、装饰、外观、享元、代理模式
    行为型对象模式：访问者、模板模式、策略模式、状态模式、观察者模式、备忘录模式、中介者模式、迭代器模式、解释器模式、命令模式、责任链模式

--- src/main/java/horse/algorithm/string/SuffixExpression.java
+++ src/main/java/horse/algorithm/string/SuffixExpression.java
@@ -16,9 +16,9 @@ public class SuffixExpression {
      *  1.根据计算顺序，对上面的式子逐层加括号。
      *  2.将每层括号内的符号移到对应外层外。
      */
-
     //中缀表达式转换后缀表达式
     //(a+b)*c-d
+    // ab+c*d-
     //-->(((a+b)*c)-d) --> ab+c*d-
 
 
@@ -27,6 +27,10 @@ public class SuffixExpression {
      * 1.对标中缀转后缀，也就是把符号提到括号前面即可。
      */
     // 后缀转换中缀表达式 ab+c*d-
+
+    //(a+b)*c-d
+
+
     // (((a+b)*c)-d)-->(a+b)*c-d
     public static void main(String[] args) {
         System.out.println(suffixExp(new String[]{"1", "2", "+", "3" , "*", "4", "-"}));
